a6cddffea2a90560bb39cad6de04689e81137425
Sergey_Kutsiy <Alfa>
2015-12-27 12:55:19 +0200

    Test commit project

--- sergey_kutsiy/src/main/java/session3/Task1.java
+++ sergey_kutsiy/src/main/java/session3/Task1.java
@@ -1,10 +1,45 @@
 package session3;
 
-/**
- * Created by Сергей on 27.12.2015.
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/*
+Из файла прочитать строку и вывести в виде массива и строки
  */
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println();
+        Path file = Paths.get("D:\\text.txt");
+        SeekableByteChannel channel = null;
+
+        try {
+            channel = Files.newByteChannel(file);
+            ByteBuffer buffer = ByteBuffer.allocate(5);
+            int readed;
+            byte[] arr = new byte[10];
+
+            while((readed = channel.read(buffer)) > 0) {
+                buffer.flip();
+                arr=buffer.array();
+                System.out.println(Arrays.toString(arr));
+                System.out.println(new String(arr));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (channel != null) {
+                try {
+                    channel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
