8013e420b6f0470ea57168ff3627d8f81ba36375
Вадим <1vaddav1@mail.ru>
2016-01-13 00:19:02 +0200

    AsyncChatTest class added hw4.chat packages (main and test removed) chat2.fxml removed AsyncChat updated

---
+++ vadim_davidenko/src/main/java/hw3/chat/AsyncChatTest.java
@@ -0,0 +1,25 @@
+package hw3.chat;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Вадим on 12.01.2016.
+ */
+public class AsyncChatTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AsyncChat chat1 = new AsyncChat("P2P Chat 1", true);
+        Stage stage1 = new Stage();
+        chat1.start(stage1);
+
+        AsyncChat chat2 = new AsyncChat("P2P Chat 2", false);
+        Stage stage2 = new Stage();
+        chat2.start(stage2);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
