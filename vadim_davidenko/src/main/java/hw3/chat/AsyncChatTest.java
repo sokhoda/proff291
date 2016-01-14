36f5b4dd251f29bf948cf7561d8712a4f91cd38c
vdavidenko <1vaddav1@mail.ru>
2016-01-14 11:38:01 +0200

    AsyncChat and AsyncChatServer changed to use Runnable interface

--- vadim_davidenko/src/main/java/hw3/chat/AsyncChatTest.java
+++ vadim_davidenko/src/main/java/hw3/chat/AsyncChatTest.java
@@ -10,13 +10,23 @@ public class AsyncChatTest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+//        AsyncChat chat1 = new AsyncChat("P2P Chat 1");
+//        Stage stage1 = new Stage();
+//        chat1.start(stage1);
+
+//        AsyncChat chat2 = new AsyncChat("P2P Chat 2");
+//        Stage stage2 = new Stage();
+//        chat2.start(stage2);
+
         AsyncChat chat1 = new AsyncChat("P2P Chat 1");
         Stage stage1 = new Stage();
-        chat1.start(stage1);
+        chat1.createStage(stage1);
+        new Thread(chat1).start();
 
         AsyncChat chat2 = new AsyncChat("P2P Chat 2");
         Stage stage2 = new Stage();
-        chat2.start(stage2);
+        chat2.createStage(stage2);
+        new Thread(chat2).start();
     }
 
     public static void main(String[] args) {
