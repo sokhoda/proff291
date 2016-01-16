718fffeb5a0554c53483d12a7f6eb32f9ceecccc
Lena <olenka_babenko@ukr.net>
2016-01-16 05:11:02 +0200

    SynchronChat_commiting

--- olena_babenko/src/main/java/ChatApp/ChatConnection.java
+++ olena_babenko/src/main/java/ChatApp/ChatConnection.java
@@ -13,9 +13,15 @@ import java.util.function.Consumer;
  */
 public abstract class ChatConnection {
     private ConnectionThread connThread = new ConnectionThread();
-
     private Consumer<Serializable> onReceiveCallBack;
-    public ChatConnection(Consumer<Serializable> onReceiveCallBack){
+
+    protected abstract boolean isServer();
+
+    protected abstract String getIP();
+
+    protected abstract int getPort();
+
+    public ChatConnection(Consumer<Serializable> onReceiveCallBack) {
         this.onReceiveCallBack = onReceiveCallBack;
         connThread.setDaemon(true);
     }
@@ -25,8 +31,6 @@ public abstract class ChatConnection {
     }
 
     public void send(String data) throws Exception {
-
-        //connThread.buffer.flip();
 
         connThread.buffer.put(data.getBytes());
         connThread.buffer.flip();
@@ -44,45 +48,41 @@ public abstract class ChatConnection {
         }
     }
 
-    protected abstract boolean isServer();
-    protected abstract String getIP();
-    protected abstract int getPort();
-
     private class ConnectionThread extends Thread {
         private SocketChannel socketChannel;
         private ByteBuffer buffer;
 
         @Override
         public void run() {
-            try {    if (isServer()) {
-                ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-                serverSocketChannel.socket().bind(new InetSocketAddress(getPort()));
-                SocketChannel socketChannel = serverSocketChannel.accept();
-                this.socketChannel = socketChannel;
-                onReceiveCallBack.accept("Server is turned on");
-            } else {SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(getIP(), getPort()));
-                this.socketChannel = socketChannel;
-                onReceiveCallBack.accept("Client is connected to Server");
-            }
+            try {
+                if (isServer()) {
+                    ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+                    serverSocketChannel.socket().bind(new InetSocketAddress(getPort()));
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    this.socketChannel = socketChannel;
+                    onReceiveCallBack.accept("Server is turned on");
+                } else {
+                    SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(getIP(), getPort()));
+                    this.socketChannel = socketChannel;
+                    onReceiveCallBack.accept("Client is connected to Server");
+                }
 
                 ByteBuffer buffer = ByteBuffer.allocate(300);
                 this.buffer = buffer;
-                //buffer.flip();
 
-                while(true){
+                while (true) {
                     socketChannel.read(buffer);
                     String str = new String(buffer.array());
                     Serializable data = (Serializable) str;
                     onReceiveCallBack.accept(data);
-                buffer.clear();
+                    buffer.clear();
                 }
-                }
-            catch (Exception e) {
+            } catch (Exception e) {
                 onReceiveCallBack.accept("Connection is closed");
             }
         }
 
 
-        }
+    }
 
 }
