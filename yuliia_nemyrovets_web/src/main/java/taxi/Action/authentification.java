51e17939bbe6f7600f793d4d66e8a206d503bf41
Юлия <nemirof1993@mail.ru>
2016-01-23 11:05:06 +0200

    Chat

---
+++ yuliia_nemyrovets_web/src/main/java/taxi/Action/authentification.java
@@ -0,0 +1,24 @@
+package taxi.Action;
+
+/**
+ * Created by Юлия on 22.01.2016.
+ */
+public class Authentification {
+    Registration registration=new Registration();
+    public boolean isClientExist(String login){
+        return registration.isExist(login);
+    }
+    public boolean isAuthentified(String login, String password){
+        if (registration.isExist(login)) {
+            String [] data= registration.getClientsData(login);
+            if(data[0].equals(password)){
+                return true;
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
