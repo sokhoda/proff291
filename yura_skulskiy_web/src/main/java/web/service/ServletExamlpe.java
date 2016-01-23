bbc5ccf3c6e47a9bc2a6e493032865c7ecd1049e
Юра <airtwist_10@mai.ru>
2016-01-23 12:45:38 +0200

    adding my proj _web

---
+++ yura_skulskiy_web/src/main/java/web/service/ServletExamlpe.java
@@ -0,0 +1,19 @@
+package web.service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Юра on 17.01.2016.
+ */@WebServlet("/x.html")
+public class ServletExamlpe extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().println("Yo nigga");
+    }
+}
