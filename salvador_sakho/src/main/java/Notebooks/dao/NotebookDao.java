0be02afcd8ff56dd3b7281c9536e26331d58d242
salvador_sakho <Salvador.Sakho21@gmail.com>
2016-02-11 14:45:11 +0200

    Merge branch 'master' of ssh://www.jon.com.ua:29418/proff29
    Problem "javafx.fxml.loadexception"

---
+++ salvador_sakho/src/main/java/Notebooks/dao/NotebookDao.java
@@ -0,0 +1,23 @@
+package Notebooks.dao;
+
+import Notebooks.domain.Notebook;
+
+import java.util.List;
+
+/**
+ * Created by User on 08.02.2016.
+ */
+public interface NotebookDao {
+
+    Long createNote(Notebook notebook);
+
+    Notebook read(Long id);
+
+    void update(Notebook notebook);
+
+    void delete(Notebook notebook);
+
+    List<Notebook> findAll();
+
+//    List<Notebook> findMonyGT(long amount);
+}
