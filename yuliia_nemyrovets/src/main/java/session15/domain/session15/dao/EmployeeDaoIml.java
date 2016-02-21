abf808644087f9cd668fbf2a4e469d3260d834ab
Юлия <nemirof1993@mail.ru>
2016-02-21 11:14:35 +0200

    Table

---
+++ yuliia_nemyrovets/src/main/java/session15/domain/session15/dao/EmployeeDaoIml.java
@@ -0,0 +1,91 @@
+package session15.domain.session15.dao;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import web.domain.*;
+import web.domain.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Юлия on 20.02.2016.
+ */
+public class EmployeeDaoIml implements EmployeeDao{
+
+    private static Logger log = Logger.getLogger(EmployeeDaoIml.class);
+    private SessionFactory factory;
+
+    public EmployeeDaoIml(SessionFactory factory) {
+        this.factory = factory;
+
+    }
+
+    @Override
+    public List findAllEmployee() {
+       // List<Employee> list = new ArrayList<>();
+
+        Session session = factory.openSession();
+        try {
+           Query query = session.createQuery("from Employee");
+           // List<Object[]> list1 = query.list();
+
+            return query.list();
+        } catch (Exception e) {
+            log.error("Transaction is being failed");
+            return null;
+        } finally {
+            session.close();
+
+        }
+    }
+
+    @Override
+    public List findAllEmployeeByDepartment(int dep_id) {
+        Session session = factory.openSession();
+        try {
+            Query query = session.createQuery("from EMPLOYEES WHERE DEPARTMENT_ID="+dep_id);
+            return query.list();
+        } catch (Exception e) {
+            log.error("Transaction is being failed");
+            return null;
+        } finally {
+            session.close();
+
+        }
+    }
+
+    @Override
+    public List findAllEmployeeWithNoDepartment() {
+        Session session = factory.openSession();
+        try {
+            Query query = session.createQuery("FROM EMPLOYEES WHERE DEPARTMENT_ID IS NULL ");
+            return query.list();
+        } catch (Exception e) {
+            log.error("Transaction is being failed");
+            return null;
+        } finally {
+            session.close();
+
+        }
+    }
+
+    public static Logger getLog() {
+        return log;
+    }
+
+    public static void setLog(Logger log) {
+        EmployeeDaoIml.log = log;
+    }
+
+    public SessionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+}
