91706d8bc3bd75fe4f83a54a93fbd8662569e2b6
lyashenko_dmitriy <solykk@yandex.ru>
2016-02-27 11:39:46 +0200

    hw7 and

--- lyashenko_dmitriy/src/main/java/hw7/notes/dao/SalesDaoImpl.java
+++ lyashenko_dmitriy/src/main/java/hw7/notes/dao/SalesDaoImpl.java
@@ -1,34 +1,94 @@
 package hw7.notes.dao;
 
-import antlr.collections.List;
-import hw7.notes.damain.Sales;
+import java.util.List;
+
+import hw7.notes.domain.Sales;
+import hw7.notes.service.NotebookServiceImpl;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 /**
  * Created by Admin on 17.02.2016.
  */
 public class SalesDaoImpl implements SalesDao {
+
+    public  SalesDaoImpl (){}
+
+    private SessionFactory sessionFactory = NotebookServiceImpl.getSessionFactory();
+
     @Override
     public Long create(Sales sales) {
-        return null;
+        Long id = null;
+        Session session = sessionFactory.openSession();
+        try{
+            session.beginTransaction();
+            id = (Long)session.save(sales);
+            session.getTransaction().commit();
+            return id;
+        } catch (HibernateException e){
+            session.getTransaction().rollback();
+            return null;
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
-    public Sales read(Long ig) {
-        return null;
+    public Sales read(Long id) {
+        Session session = sessionFactory.openSession();
+        try {
+            return (Sales) session.get(Sales.class, id);
+        } catch (HibernateException e){
+            return null;
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
     public boolean update(Sales sales) {
-        return false;
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.update(sales);
+            session.getTransaction().commit();
+            return true;
+        } catch (HibernateException e){
+            session.getTransaction().rollback();
+            return false;
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
     public boolean delete(Sales sales) {
-        return false;
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.delete(sales);
+            session.getTransaction().commit();
+            return true;
+        } catch (HibernateException e){
+            session.getTransaction().rollback();
+            return false;
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
     public List findAll() {
-        return null;
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("from hw7.notes.domain.Sales");
+        session.close();
+        sessionFactory.close();
+        return query.list();
     }
 }
