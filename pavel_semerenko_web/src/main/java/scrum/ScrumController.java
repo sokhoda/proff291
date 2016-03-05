fdde20e48bfe9330e16a6a4ee71dd2fdc2af11bb
lidia.gaidukova <lidiya.gaidukova@gmail.com>
2016-03-05 13:04:42 +0200

    first

--- pavel_semerenko_web/src/main/java/scrum/ScrumController.java
+++ pavel_semerenko_web/src/main/java/scrum/ScrumController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
+jj
 //import web.service.EmployeeService;
 /**
  * Created by Pavel on 05.03.2016.
