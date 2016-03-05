6aa0d4b883290a86b677dba9b1d7c702ea0b4c95
lyashenko_dmitriy <solykk@yandex.ru>
2016-03-05 13:17:59 +0200

    update

--- alexandr_khoda_web/src/main/java/Scrum/controller/Service/ServiceImpl.java
+++ alexandr_khoda_web/src/main/java/Scrum/controller/Service/ServiceImpl.java
@@ -13,7 +13,6 @@ public class ServiceImpl implements Service {
 
     @Override
     public String[] stringToArray(String string) {
-
         String [] array = string.split("[ ]+");
         return array;
     }
@@ -28,16 +27,16 @@ public class ServiceImpl implements Service {
     }
 
     @Override
-    public String revers(String[] strings) {
-        String temp = "";
-        for (int i = strings.length - 1 ; i >= 0; i--){
-            temp += strings[i] + " ";
+    public String[] revers(String[] strings) {
+        String[] temp = new String[strings.length];
+        for (int i = 0; i < 0; i++){
+            temp[i] = strings[(strings.length - 1) - i];
         }
         return temp;
     }
 
     @Override
-    public String random(String[] strings) {
+    public String[] random(String[] strings) {
         List<String> tepmArray = new ArrayList<String>();
         for (int i = 0; i < strings.length; i++){
             tepmArray.add(strings[i]);
@@ -47,7 +46,7 @@ public class ServiceImpl implements Service {
     }
 
     @Override
-    public String ArrToStr(String[] str) {
+    public String arrToStr(String[] str) {
         String string="";
        for(int i =0; i<str.length;i++){
            string = string+ str[i]+" ";
