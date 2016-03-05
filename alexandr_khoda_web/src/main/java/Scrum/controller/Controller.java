51c49f76aaef8dc52903d463bdaadb8cf540fd02
Юлия <nemirof1993@mail.ru>
2016-03-05 12:50:22 +0200

    create spring module

--- alexandr_khoda_web/src/main/java/Scrum/controller/Controller.java
+++ alexandr_khoda_web/src/main/java/Scrum/controller/Controller.java
@@ -1,7 +1,27 @@
 package Scrum.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 /**
  * Created by s_okhoda on 05.03.2016.
  */
+@org.springframework.stereotype.Controller
+@SessionAttributes("controller")
 public class Controller {
+    public static final Logger log = Logger.getLogger(Controller.class);
+
+
+    @RequestMapping(value = "/hello.html", method = RequestMethod.GET)
+    public String hello(@RequestParam("first") String sum, @RequestParam("second")  String reverse, @RequestParam("third") String rand, Model model) {
+        log.info("/hello.html controller");
+        model.addAttribute("sum" + sum+"/n"+"reverse"+reverse+"random"+rand);
+        return "index";
+    }
+
 }
