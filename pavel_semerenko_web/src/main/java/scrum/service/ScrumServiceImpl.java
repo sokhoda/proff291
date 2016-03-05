d88d902c6e3ce761b083e1cc390915f00a7353f5
devel <vladymyr.danilov@gmail.com>
2016-03-05 13:00:05 +0200

    Create Service in Scrum project

---
+++ pavel_semerenko_web/src/main/java/scrum/service/ScrumServiceImpl.java
@@ -0,0 +1,24 @@
+package scrum.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class ScrumServiceImpl implements ScrumService {
+    @Override
+    public List<Integer> StringToArray(String string) {
+        List<Integer> array = new ArrayList<>();
+        String[] items = string.split(" ");
+
+        for ( int i = 0; i < items.length; i++ ) {
+           try {
+               array.add(Integer.parseInt(items[i]));
+           } catch (Exception e) {
+               return null;
+           }
+        }
+    }
+}
