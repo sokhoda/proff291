be54b44100fdc648720bcb8dc64b4234e22bce71
dima_rutskiy <drutskey@gmail.com>
2016-03-05 13:12:37 +0200

    main

---
+++ vadim_davidenko_web/src/main/java/agile/service/Main.java
@@ -0,0 +1,22 @@
+package agile.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Locale;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al1
+ * Date: 15.11.15
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        NumberService serv=new NumberServiceImpl();
+        String text=("1 2 3 4 5");
+        System.out.println("text "+ text);
+        System.out.println("shuffle "+serv.shuffle(text));
+
+}
+}
