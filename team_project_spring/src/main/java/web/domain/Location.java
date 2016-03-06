7fb10f978cb122254e0acd79f43942c984255719
devel <vladymyr.danilov@gmail.com>
2016-03-06 12:29:14 +0200

    Add Location entity to team_project_spring

--- team_project_spring/src/main/java/web/domain/Location.java
+++ team_project_spring/src/main/java/web/domain/Location.java
@@ -1,13 +1,120 @@
 package web.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-/**
- * Created by lenchi on 06.03.16.
- */
 @Entity
 @Table(name = "LOCATIONS")
+@SequenceGenerator(name = "sequence", sequenceName = "LOCATION_SEQ")
 public class Location {
-    /*...*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOCATION_SEQ")
+    @Column(name = "LOCATION_ID")
+    private Long id;
+
+    @Column(name = "STREET_ADDRESS")
+    private String streetAddress;
+
+    @Column(name = "POSTAL_CODE")
+    private String postalCode;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "STATE_PROVINCE")
+    private String stateProvince;
+
+    @Column(name = "COUNTRY_ID")
+    private Long countryId;
+
+    public Location() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    public Long getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Long countryId) {
+        this.countryId = countryId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (id != null ? !id.equals(location.id) : location.id != null) return false;
+        if (streetAddress != null ? !streetAddress.equals(location.streetAddress) : location.streetAddress != null)
+            return false;
+        if (postalCode != null ? !postalCode.equals(location.postalCode) : location.postalCode != null) return false;
+        if (city != null ? !city.equals(location.city) : location.city != null) return false;
+        if (stateProvince != null ? !stateProvince.equals(location.stateProvince) : location.stateProvince != null)
+            return false;
+        return countryId != null ? countryId.equals(location.countryId) : location.countryId == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (streetAddress != null ? streetAddress.hashCode() : 0);
+        result = 31 * result + (postalCode != null ? postalCode.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (stateProvince != null ? stateProvince.hashCode() : 0);
+        result = 31 * result + (countryId != null ? countryId.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", streetAddress='" + streetAddress + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                ", stateProvince='" + stateProvince + '\'' +
+                ", countryId=" + countryId +
+                '}';
+    }
 }
