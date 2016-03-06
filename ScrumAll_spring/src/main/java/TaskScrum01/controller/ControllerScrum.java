f194dd3b09f2ab7fc2ee41262595d214402267e6
alexandr_khoda <oleksandr.khodakovskyi@gmail.com>
2016-03-06 11:52:27 +0200

    ScrumAll_spring

---
+++ ScrumAll_spring/src/main/java/TaskScrum01/controller/ControllerScrum.java
@@ -0,0 +1,27 @@
+package TaskScrum01.controller;
+
+import Scrum.controller.Service.ServiceClass;
+import Scrum.exception.StringDataException;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by s_okhoda on 05.03.2016.
+ */
+@org.springframework.stereotype.Controller
+public class ControllerScrum {
+    public static final Logger log = Logger.getLogger(ControllerScrum.class);
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public String index(Model model) {
+        log.info("/index Controller");
+        return "index";
+    }
+
+
+
+}
